4b0f3f5e0a2a32136da723dbfed47489efa91e7a
Kraken Jr. IoT - Arduino Automation Plaform <55251869+krakenjriot@users.noreply.github.com>
2020-04-03 08:29:57 +0300

    Add files via upload

---
+++ fetch_1.php
@@ -0,0 +1,123 @@
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['line']});
+      google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('datetime', "Time");
+      data.addColumn('number', 'Temperature');
+      //data.addColumn('number', 'Sensor2');
+      
+
+      data.addRows([
+   		<?php
+			
+			date_default_timezone_set("Asia/Riyadh"); 		
+			
+			$connect = new PDO("mysql:host=localhost;dbname=mydb", "root", "");
+
+			/*****************************************************************/
+			/*****************************************************************/
+			/*****************************************************************/
+	
+			/*****************************************************************/
+			/*****************************************************************/
+			/*****************************************************************/
+
+
+			 //$query = " SELECT dtime,sensor1,sensor2,hum,temp  FROM tbl_sensors ORDER BY id DESC LIMIT 100 ";			 
+			 $query = " SELECT *  FROM tbl_sensors ORDER BY id DESC LIMIT 500 ";			 
+			 
+			 $statement = $connect->prepare($query);
+			 $statement->execute();
+			 $result = $statement->fetchAll();
+
+			//$i = 0;
+			foreach($result as $row)
+				{
+					//++$i; 
+					$time = strtotime($row['dtime']);
+					//$tzone = strtotime($row['tzone']);
+
+					
+					$y = date("Y",$time);
+					$mo = date("m",$time);
+					$d = date("d",$time);
+					
+					$h = date("H",$time);
+					$m = date("i",$time);
+					$s = date("s",$time);	
+					$u = date("u",$time);	
+					
+								
+					$sensor1 = $row['temp'];
+					
+						
+					if($sensor1 == 0) continue;
+					
+					
+					echo "[new Date($y,$mo,$d,$h,$m,$s), $sensor1],";
+				}
+
+					$y = date("Y");
+					$mo = date("m");
+					$d = date("d");					
+					$h = date("H");										
+					$m = date("i");										
+					$s = date("s");	
+
+						
+		?>	
+
+		
+      ]);
+
+      var options = {
+        chart: {
+          title: 'Ambient Temperature Graph (c)',
+          subtitle: 'Time/Date: <?php echo date('hA (M-d-Y )',time()); ?>'
+        },		  
+        width: 900,
+        height: 500,
+        legend: {position: 'top'},
+        enableInteractivity: false,
+        chartArea: {
+          width: '100%'
+        },
+        hAxis: {
+          viewWindow: {
+            //min: new Date(2020, 4, 1, 8, ),
+            //max: new Date(2020, 4, 1, 8, 49)            
+			
+			//min: new Date(<?php echo $y.",".$mo.",".$d.",".$h.",".$m.",".($s-60); ?>),
+			min: new Date(<?php echo $y.",".$mo.",".$d.",".$h.",".($m-30); ?>),			
+            max: new Date(<?php echo $y.",".$mo.",".$d.",".$h.",".($m); ?>),
+			
+			//min: new Date(<?php echo $y.",".$mo.",".$d; ?>,9,0),			
+            //max: new Date(<?php echo $y.",".$mo.",".$d; ?>,9,59),			
+          },
+          gridlines: {
+            count: -1,
+            units: {
+              days: {format: ['MMM dd']},
+              hours: {format: ['HH:mm', 'ha']},
+            }
+          },
+          minorGridlines: {
+            units: {
+              hours: {format: ['hh:mm:ss a', 'ha']},
+              minutes: {format: ['HH:mm a Z', ':mm']}
+            }
+          }
+        }
+      };
+
+      var chart = new google.charts.Line(document.getElementById('line_top_x'));
+	  
+      chart.draw(data, google.charts.Line.convertOptions(options));
+    }	
+  </script>
+  
+  <div id="line_top_x"></div>
